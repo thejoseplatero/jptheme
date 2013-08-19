e75bbe89f99b22fc0010ee4893c98f9cc59b8365
Jose Platero <contact@joseplatero.com>
2013-08-19 12:49:31 -0400

    added ipsom lorem into bands

--- t_home.php
+++ t_home.php
@@ -3,17 +3,34 @@
 
 <?php get_header(); ?>
 
-<div class="band">
-  
+<!--Why I Do It -->
+<div class="band why">
+	<div class="copy">
+		<?php $recent = new WP_Query("page_id=36"); 
+		while($recent->have_posts()) : $recent->the_post();?>
+	 		<?php the_content(); ?>	
+	 	<?php endwhile; ?>
+ 	</div>
 </div>
 
-<div class="band">
-  
+
+<div class="band how">
+	<div class="copy">
+		<?php $recent = new WP_Query("page_id=38"); 
+		while($recent->have_posts()) : $recent->the_post();?>
+	 		<?php the_content(); ?>	
+	 	<?php endwhile; ?>
+ 	</div>
 </div>
 
+<div class="band what">
+	<div class="copy">
+		<?php $recent = new WP_Query("page_id=41"); 
+		while($recent->have_posts()) : $recent->the_post();?>
+	 		<?php the_content(); ?>	
+	 	<?php endwhile; ?>
+	 </div>
+</div>
 
 
 <?php get_footer(); ?>
-
-
-
