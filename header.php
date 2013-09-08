7af747c8355311d8430301bc6b4caffc793871ae
Jose Platero <contact@joseplatero.com>
2013-09-07 21:27:10 -0400

    copy updated

--- header.php
+++ header.php
@@ -39,17 +39,13 @@
    <!-- 
            <?php wp_nav_menu(array('menu' => 'main' )); ?>  -->
      
- <div class="band header">
-<!--    <div class="video-container">
-     <video id="#background_video" height="1180px" autoplay="true" loop="loop" onended="this.play()" muted poster="/jose/assets/josebanner.jpg">
-       <source src="/jose/assets/canada_v2.1.mp4" type="video/mp4">
-     </video>
-   </div> -->
- <!--  <h1>Catch Phrase<h1> -->
-<!--   <div class="jose-image-container">
-      
-  </div> -->
-    
+ <div class='band header'>
+  <div class="copy">
+    <?php $recent = new WP_Query("page_id=41"); 
+    while($recent->have_posts()) : $recent->the_post();?>
+      <?php the_content(); ?> 
+    <?php endwhile; ?>
+  </div>
  </div>
 
 
--- t_home.php
+++ t_home.php
@@ -3,9 +3,10 @@
 
 <?php get_header(); ?>
 
-<!--Why I Do It -->
-<div class="band why">
+
+<div class="band " id="bouncing-ball">
 	<div class="copy">
+		<img class='bouncing-image' src="/jose/assets/ball3.svg" alt="ball">	
 		<?php $recent = new WP_Query("page_id=36"); 
 		while($recent->have_posts()) : $recent->the_post();?>
 	 		<?php the_content(); ?>	
@@ -14,34 +15,23 @@
 </div>
 
 
-<div class="band how">
+
+<div class="band" id='still-bouncing'>	
 	<div class="copy">
+		<img class='still-bouncing-image' src="/jose/assets/still-bounce.svg" alt="flyingman">
 		<?php $recent = new WP_Query("page_id=38"); 
 		while($recent->have_posts()) : $recent->the_post();?>
 	 		<?php the_content(); ?>	
 	 	<?php endwhile; ?>
+	 	<div class="contact">
+	 	    <a href="mailto:contact@joseplatero" class="button">Email</a>
+	 	    <a href="twitter.com/thejoseplatero" class="button">Twitter</a>
+	 	    <a href="http://www.linkedin.com/in/joseplatero" class="button">LinkedIn</a>
+	 	    <a href="https://github.com/thejoseplatero/jptheme" class="button">Github</a>
+	 	    <a href="#" class="button">Blog</a>
+	 	</div>
  	</div>
+
 </div>
-
-<div class="band what">
-	<div class="video-container-small">
-		
-	</div>
-
-	<div class="copy">
-		<?php $recent = new WP_Query("page_id=41"); 
-		while($recent->have_posts()) : $recent->the_post();?>
-	 		<?php the_content(); ?>	
-	 	<?php endwhile; ?>
-	</div>
-	<div class="contact">
-			<a href="mailto:contact@joseplatero" class="button">Email</a>
-			<a href="twitter.com/thejoseplatero" class="button">Twitter</a>
-			<a href="http://www.linkedin.com/in/joseplatero" class="button">LinkedIn</a>
-			<a href="https://github.com/thejoseplatero/jptheme" class="button">Github</a>
-			<a href="#" class="button">Blog</a>
-	</div>
-</div>
-
 
 <?php get_footer(); ?>
