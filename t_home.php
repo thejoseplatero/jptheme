f96523d84887b5d9991110e0eb0abed118d2d83b
Jose Platero <contact@joseplatero.com>
2013-11-01 21:28:37 -0400

    change layout, two columns

--- t_home.php
+++ t_home.php
@@ -5,22 +5,26 @@
 
 	<div class="band" id="bouncing-ball">
 		<div class="copy">
-			<img class='bouncing-image' src="<?php bloginfo('url'); ?>/assets/ball3.svg" alt="ball">	
-		 		<?php $recent = new WP_Query("page_id=36"); 
+				<?php $recent = new WP_Query("page_id=36"); 
 		 		while($recent->have_posts()) : $recent->the_post();?>
 		 	 		<?php the_content(); ?>	
 		 	 	<?php endwhile; ?>
 	 	</div>
+	 	<div class="bounce-container">
+	 		<img class='bouncing-image' src="<?php bloginfo('url'); ?>/assets/ball3.svg" alt="ball">	
+	 	</div>	
 	</div>
 
 
 	<div class="band alt-band" id='still-bouncing'>	
 		<div class="copy">
-			<img class='still-bouncing-image' src="<?php bloginfo('url'); ?>/assets/still-bounce.svg" alt="flyingman">	
 				<?php $recent = new WP_Query("page_id=38"); 
 				while($recent->have_posts()) : $recent->the_post();?>
 			 		<?php the_content(); ?>	
 			 	<?php endwhile; ?>
+	 	</div>
+	 	<div class="bounce-container">
+	 		<img class='still-bouncing-image' src="<?php bloginfo('url'); ?>/assets/still-bounce.svg" alt="flyingman">	
 	 	</div>
 	</div>
 
